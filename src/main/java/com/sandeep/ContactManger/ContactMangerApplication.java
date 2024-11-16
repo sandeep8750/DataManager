c3bc2d722a090eb663257e720c1a30a3fdaffd76
sandeep8750 <monu875036@gmail.com>
2024-11-16 23:27:51 +0530

    first commit

---
+++ src/main/java/com/sandeep/ContactManger/ContactMangerApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.ContactManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandeep/ContactManger/ContactMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.ContactManger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
